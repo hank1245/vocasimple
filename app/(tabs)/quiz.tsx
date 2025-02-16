308b68cb85a242998cf49cb2df49e7df869caea5
hank <hank29206880@gmail.com>
2025-02-16 22:49:38 +0900

    style: change Quiz screen style

--- app/(tabs)/quiz.tsx
+++ app/(tabs)/quiz.tsx
@@ -1,58 +1,63 @@
+import { Colors } from "@/constants/Colors";
 import React from "react";
 import {
   SafeAreaView,
   View,
-  Text,
   StyleSheet,
   TouchableOpacity,
   Image,
 } from "react-native";
+import AppText from "@/components/common/AppText";
 import { Ionicons } from "@expo/vector-icons";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 const QuizScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Quiz</Text>
+      <View style={styles.padding}>
+        <AppText style={styles.title} text="Quiz" />
 
-      <View style={styles.gridContainer}>
-        <TouchableOpacity style={styles.card}>
-          <Ionicons name="list" size={24} color="white" />
-          <Text style={styles.cardText}>뜻 맞추기</Text>
-        </TouchableOpacity>
+        <View style={styles.gridContainer}>
+          <TouchableOpacity style={styles.card}>
+            <Ionicons name="list" size={34} color="white" />
+            <AppText style={styles.cardText} text="뜻 맞추기" />
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card}>
-          <Ionicons name="text" size={24} color="white" />
-          <Text style={styles.cardText}>단어 맞추기</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
+            <Ionicons name="text" size={34} color="white" />
+            <AppText style={styles.cardText} text="단어 맞추기" />
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card}>
-          <MaterialCommunityIcons
-            name="cards-outline"
-            size={24}
-            color="white"
+          <TouchableOpacity style={styles.card}>
+            <MaterialCommunityIcons
+              name="cards-outline"
+              size={34}
+              color="white"
+            />
+            <AppText style={styles.cardText} text="플래시카드" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.card}>
+            <Ionicons name="play" size={34} color="white" />
+            <AppText style={styles.cardText} text="자동재생" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.progressContainer}>
+          <AppText
+            style={styles.progressText}
+            text="매일 퀴즈를 달성하고 불꽃을 밝혀세요!"
           />
-          <Text style={styles.cardText}>플래시카드</Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card}>
-          <Ionicons name="play" size={24} color="white" />
-          <Text style={styles.cardText}>자동재생</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.progressContainer}>
-        <Text style={styles.progressText}>
-          매일 퀴즈를 달성하고 불꽃을 밝혀세요!
-        </Text>
-
-        <Image
-          source={require("../../assets/images/flame.png")}
-          style={styles.flameIcon}
-        />
-        <Text style={styles.progressSubText}>
-          이번 달 획득한 불꽃: <Text style={styles.boldText}>4/30</Text>
-        </Text>
+          <Image
+            source={require("../../assets/images/flame.png")}
+            style={styles.flameIcon}
+          />
+          <AppText
+            style={styles.progressSubText}
+            text={`이번 달 획득한 불꽃: 4/30`}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -62,13 +67,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingHorizontal: 16,
-    paddingTop: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: "bold",
-    marginBottom: 20,
+    marginTop: 20,
+    marginBottom: 36,
+  },
+  padding: {
+    paddingHorizontal: 30,
   },
   gridContainer: {
     flexDirection: "row",
@@ -77,38 +84,40 @@ const styles = StyleSheet.create({
   },
   card: {
     width: "48%",
-    aspectRatio: 1.5,
-    backgroundColor: "#6A5ACD",
+    height: 100,
+    backgroundColor: Colors.primary,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 10,
+    borderRadius: 15,
     marginBottom: 10,
   },
   cardText: {
     color: "white",
-    fontSize: 16,
+    fontSize: 15,
     marginTop: 5,
+    fontWeight: "bold",
   },
   progressContainer: {
-    marginTop: 30,
-    backgroundColor: "#f0f0f0",
-    padding: 20,
-    borderRadius: 10,
+    marginTop: 50,
+    backgroundColor: "#EDF0F3",
+    paddingHorizontal: 20,
+    borderRadius: 20,
     alignItems: "center",
+    justifyContent: "center",
+    height: "40%",
   },
   progressText: {
-    fontSize: 14,
+    fontSize: 16,
     fontWeight: "bold",
   },
   flameIcon: {
-    width: 50,
-    height: 50,
-    marginVertical: 10,
+    width: 82,
+    height: 89,
+    marginTop: 30,
+    marginBottom: 25,
   },
   progressSubText: {
     fontSize: 16,
-  },
-  boldText: {
     fontWeight: "bold",
   },
 });
