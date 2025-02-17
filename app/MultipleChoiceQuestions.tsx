571176571e481cd7542553c53733204212ecbda1
hank <hank29206880@gmail.com>
2025-02-17 17:11:22 +0900

    chore: change UI when option is selected

--- app/MultipleChoiceQuestions.tsx
+++ app/MultipleChoiceQuestions.tsx
@@ -12,6 +12,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter, useLocalSearchParams } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import { Dimensions } from "react-native";
+const windowWidth = Dimensions.get("window").width;
 
 const meaningQuestions = [
   {
@@ -85,6 +87,7 @@ const QuizTab = () => {
   const questions = mode === "word" ? wordQuestions : meaningQuestions;
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [progress, setProgress] = useState(new Animated.Value(0));
+  const [selected, setSelected] = useState(null);
 
   const handleContinue = () => {
     if (currentQuestionIndex < questions.length - 1) {
@@ -103,9 +106,25 @@ const QuizTab = () => {
     <View style={styles.questionContainer}>
       <Text style={styles.questionText}>{item.question}</Text>
       {item.options.map((option: any, idx: number) => (
-        <TouchableOpacity key={idx} style={styles.optionButton}>
+        <TouchableOpacity
+          key={idx}
+          style={[
+            styles.optionButton,
+            selected === idx && { borderColor: Colors.primary },
+          ]}
+          onPress={() => setSelected(idx)}
+        >
           <Text style={styles.optionText}>{option}</Text>
-          <View style={styles.select}></View>
+          <View
+            style={[
+              styles.select,
+              selected === idx && { borderColor: Colors.primary },
+            ]}
+          >
+            {selected === idx ? (
+              <View style={styles.selectedDot}></View>
+            ) : undefined}
+          </View>
         </TouchableOpacity>
       ))}
       <TouchableOpacity style={styles.continueButton} onPress={handleContinue}>
@@ -144,8 +163,8 @@ const QuizTab = () => {
           showsHorizontalScrollIndicator={false}
           initialScrollIndex={currentQuestionIndex}
           getItemLayout={(data, index) => ({
-            length: 360,
-            offset: 360 * index,
+            length: windowWidth,
+            offset: windowWidth * index,
             index,
           })}
         />
@@ -160,7 +179,7 @@ const styles = StyleSheet.create({
   },
   goBackButton: {
     marginLeft: 20,
-    marginBottom: 20,
+    marginBottom: 24,
   },
   goBackText: {
     fontSize: 16,
@@ -175,7 +194,7 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: "#D1DBE8",
     borderRadius: 5,
-    marginBottom: 16,
+    marginBottom: 40,
   },
   progressBar: {
     height: "100%",
@@ -184,13 +203,13 @@ const styles = StyleSheet.create({
   },
   questionContainer: {
     width: 360,
-    padding: 16,
   },
   questionText: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 30,
     color: "black",
+    paddingLeft: 14,
   },
   optionButton: {
     paddingVertical: 16,
@@ -215,13 +234,22 @@ const styles = StyleSheet.create({
     borderRadius: "50%",
     position: "relative",
   },
+  selectedDot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: Colors.primary,
+    position: "absolute",
+    top: 6,
+    left: 6,
+  },
   continueButton: {
     position: "absolute",
     bottom: 10,
     width: "100%",
     alignSelf: "center",
     marginTop: 16,
-    padding: 10,
+    padding: 13,
     backgroundColor: Colors.primary,
     borderRadius: 15,
     alignItems: "center",
