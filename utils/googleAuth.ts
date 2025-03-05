57b807e2ead2c52fed254768b7471d36fcaeb37f
hank <hank29206880@gmail.com>
2025-03-05 15:56:50 +0900

    feat: add webClientId

--- utils/googleAuth.ts
+++ utils/googleAuth.ts
@@ -6,6 +6,7 @@ import { supabase } from "@/utils/supabase";
 
 GoogleSignin.configure({
   iosClientId: process.env.GOOGLE_CLOUD_CLIENT_ID_IOS,
+  webClientId: process.env.GOOGLE_CLOUD_CLIENT_ID_WEB,
   scopes: ["https://www.googleapis.com/auth/drive.readonly"],
 });
 
