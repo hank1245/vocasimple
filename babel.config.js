a5cc6ae10afc7af1e28a6fa4cc17b04a63e927b7
hank <hank29206880@gmail.com>
2025-08-24 18:39:51 +0100

    chore: change babel settiongs

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const plugins = [require.resolve("expo-router/babel")];
+
+  if (process.env.NODE_ENV === "production") {
+    plugins.push(["transform-remove-console", { exclude: ["error", "warn"] }]);
+  }
+
+  // Reanimated plugin must be listed last
+  plugins.push("react-native-reanimated/plugin");
+
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
-    env: {
-      production: {
-        plugins: [["transform-remove-console", { exclude: ["error", "warn"] }]],
-      },
-    },
+    plugins,
   };
 };
